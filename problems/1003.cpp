6de0caa1e5763fbc661dbb53a37e4dcb370df8e3
Juseong Park <juseongkr@gmail.com>
2019-03-26 20:44:26 +0900

    Clean up algorithm

--- problems/1003.cpp
+++ problems/1003.cpp
@@ -6,21 +6,17 @@ int dp[41];
 int fibonacci(int n) {
 	if (n == 0)
 		return 0;
-	else if (n == 1)
+	if (n == 1)
 		return 1;
+	if (dp[n] != 0)
+		return dp[n];
 
-	if (dp[n] == 0) {
-		dp[n] = fibonacci(n-1) + fibonacci(n-2);
-		return dp[n];
-	} else {
-		return dp[n];
-	}
+	return dp[n] = fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main()
 {
 	int num, n;
-	dp[0] = 0;
 	dp[1] = 1;
 
 	cin >> num;
