a49df1456819c18c47520a8a694456cb3d3863d1
Juseong Park <juseongkr@gmail.com>
2019-04-03 21:00:39 +0900

    STL priority queue for beginner (min heap)

---
+++ acmicpc/1927.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	priority_queue<int, vector<int>, greater<int>> que;
+	int n, m;
+
+	scanf("%d", &n);
+	for (int i=0; i<n; ++i) {
+		scanf("%d", &m);
+		if (m == 0) {
+			if (que.empty()) {
+				printf("0\n");
+			} else { 
+				printf("%d\n", que.top());
+				que.pop();
+			}
+		} else {
+			que.push(m);
+		}
+	}
+	return 0;
+}
