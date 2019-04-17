21cac4b57cdfe8093e7f3cffe0460821f7fe29ee
Juseong Park <juseongkr@gmail.com>
2019-04-18 02:54:31 +0900

    Calculate partial sum using Segment Tree

---
+++ acmicpc/2042.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+long long num[2000001], tree[4000002];
+long long a, b, c;
+int n, m, k;
+
+long long __make(int cur, int left, int right)
+{
+	if (left == right) {
+		tree[cur] = num[right];
+		return tree[cur];
+	}
+
+	int mid = (left + right)/2;
+	tree[cur] = __make(2*cur, left, mid) + \
+		    __make(2*cur+1, mid+1, right);
+	return tree[cur];
+}
+
+void __update(int cur, int left, int right, int idx, long long diff)
+{
+	if (idx < left || right < idx)
+		return;
+
+	tree[cur] += diff;
+	if (left != right) {
+		int mid = (left + right)/2;
+		__update(2*cur, left, mid, idx, diff);
+		__update(2*cur+1, mid+1, right, idx, diff);
+	}
+}
+
+long long __sum(int cur, int left, int right, int start, int end)
+{
+	if (end < left || right < start)
+		return 0;
+
+	if (start <= left && right <= end)
+		return tree[cur];
+
+	int mid = (left + right)/2;
+	return __sum(2*cur, left, mid, start, end) + \
+		__sum(2*cur+1, mid+1, right, start, end);
+}
+
+void make()
+{
+	__make(1, 1, n);
+}
+
+void update(int i, long long d)
+{
+	__update(1, 1, n, i, d);
+}
+
+long long sum(int s, int e)
+{
+	return __sum(1, 1, n, s, e);
+}
+
+int main()
+{
+	scanf("%d %d %d", &n, &m, &k);
+	for (int i=1; i<=n; ++i)
+		scanf("%lld", &num[i]);
+
+	make();
+	for (int i=0; i<m+k; ++i) {
+		scanf("%lld %lld %lld", &a, &b, &c);
+		if (a == 1) {
+			long long diff = c - num[b];
+			update(b, diff);
+			num[b] = c;
+		} else {
+			long long ans = sum(b, c);
+			printf("%lld\n", ans);
+		}
+	}
+
+	return 0;
+}
