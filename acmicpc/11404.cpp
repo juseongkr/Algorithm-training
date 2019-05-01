a21e05e10d63b76da293096ba79d681c7c0d6385
Juseong Park <juseongkr@gmail.com>
2019-05-01 14:04:32 +0900

    Source code clean-up

--- acmicpc/11404.cpp
+++ acmicpc/11404.cpp
@@ -1,46 +1,34 @@
 #include <iostream>
-#include <limits.h>
 using namespace std;
 
-int city[101][101];
-int n, m;
-
-void solve()
-{
-	for (int i=1; i<=n; ++i) {
-		for (int j=1; j<=n; ++j) {
-			for (int k=1; k<=n; ++k) {
-				if (city[j][i] != INT_MAX && city[i][k] != INT_MAX)
-					city[j][k] = min(city[j][k], city[j][i] + city[i][k]);
-			}
-		}
-	}
-}
+#define MAX_VAL 100000000
 
 int main()
 {
-	int a, b, w;
+	int dist[101][101];
+	int n, m, a, b, w;
 	
 	scanf("%d %d", &n, &m);
-
-	for (int i=1; i<=n; ++i)
-		for (int j=1; j<=n; ++j)
-			city[i][j] = INT_MAX;
+	for (int i=0; i<n; ++i)
+		for (int j=0; j<n; ++j)
+			dist[i][j] = MAX_VAL;
 
 	for (int i=0; i<m; ++i) {
 		scanf("%d %d %d", &a, &b, &w);
-		if (city[a][b] > w)
-			city[a][b] = w;
+		dist[a-1][b-1] = min(dist[a-1][b-1], w);
 	}
 
-	solve();
+	for (int k=0; k<n; ++k)
+		for (int i=0; i<n; ++i)
+			for (int j=0; j<n; ++j)
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 
-	for (int i=1; i<=n; ++i) {
-		for (int j=1; j<=n; ++j) {
-			if (i == j || city[i][j] == INT_MAX)
+	for (int i=0; i<n; ++i) {
+		for (int j=0; j<n; ++j) {
+			if (i == j || dist[i][j] == MAX_VAL)
 				printf("0 ");
 			else
-				printf("%d ", city[i][j]);
+				printf("%d ", dist[i][j]);
 		}
 		printf("\n");
 	}
