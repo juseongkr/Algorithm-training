6549f1b3406193ff97dff8c324689ef622b9bc9c
Juseong Park <juseongkr@gmail.com>
2019-05-01 13:40:24 +0900

    Find a shortest path using Bellman-Ford Algorithm

---
+++ acmicpc/11657.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+#define MAX_VAL 501
+
+int main()
+{
+	vector<pair<int, int>> map[MAX_VAL];
+	int dist[MAX_VAL];
+	int n, m, a, b, w;
+	bool cycle = false;
+
+	scanf("%d %d", &n, &m);
+	for (int i=0; i<m; ++i) {
+		scanf("%d %d %d", &a, &b, &w);
+		map[a-1].push_back({b-1, w});
+	}
+
+	fill(dist, dist+n, INT_MAX);
+	dist[0] = 0;
+
+	for (int k=0; k<n; ++k) {
+		for (int i=0; i<n; ++i) {
+			for (int j=0; j<map[i].size(); ++j) {
+				int next = map[i][j].first;
+				int cost = map[i][j].second;
+
+				if (dist[i] != INT_MAX && dist[next] > dist[i] + cost) {
+					if (k == n-1)
+						cycle = true;
+					dist[next] = dist[i] + cost;
+				}
+			}
+		}
+	}
+
+	if (cycle) {
+		printf("-1\n");
+	} else {
+		for (int i=1; i<n; ++i) {
+			if (dist[i] != INT_MAX)
+				printf("%d\n", dist[i]);
+			else
+				printf("-1\n");
+		}
+	}
+
+	return 0;
+}
