cd25adfdfd97baaf1a7363a9f1639078a52022af
Juseong Park <juseongkr@gmail.com>
2019-05-11 14:32:39 +0900

    Codeforces Round #549 (Div. 2)

---
+++ codeforces/Codeforces_Round_549_Div2/queen.cpp
@@ -0,0 +1,113 @@
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <tuple>
+#include <vector>
+using namespace std;
+
+#define ll		long long
+#define ull		unsigned long long
+#define fi		first
+#define se		second
+#define pb		push_back
+#define all(a)		(a).begin(), (a).end()
+#define sz(a)		((int)(a.size()))
+#define REP(i, n)	for (int i=0; i<(n); ++i)
+#define FOR(i, a, b)	for (int i=(a); i<=(b); ++i)
+#define FORR(i, a, b)	for (int i=(a); i>=(b); --i)
+
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+
+const int mod = 1e9+7;
+const ll inf = 1ll<<55;
+const ld pi = 3.1415926535897932384626433;
+const int dy[4] = {0, 1, 0, -1};
+const int dx[4] = {1, 0, -1, 0};
+const int ddy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+const int ddx[8] = {0, 0, 1, 1, 1, -1, -1, -1};
+
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { return (a*b)/GCD(a, b); }
+
+int check[100001];
+vector<int> graph[100001], ans;
+
+void dfs(int n)
+{
+	int cnt = 0;
+	for (int i=0; i<graph[n].size(); ++i) {
+		int next = graph[n][i];
+		if (check[next])
+			cnt++;
+		
+		dfs(next);
+	}
+	if (check[n] && cnt == graph[n].size())
+		ans.push_back(n);
+}
+
+int main()
+{
+
+#ifdef DEBUG_MODE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, t, s;
+
+	cin >> n;
+	for (int i=1; i<=n; ++i) {
+		cin >> t >> check[i];
+		if (t == -1)
+			s = i;
+		else
+			graph[t].push_back(i);
+	}
+
+	dfs(s);
+
+	if (ans.size()) {
+		sort(ans.begin(), ans.end());
+		for (int i=0; i<ans.size(); ++i)
+			cout << ans[i] << " ";
+	} else {
+		cout << "-1\n";
+	}
+
+	return 0;
+}
