00ecf01b2657b5dc3a9c32857904b8f66b784adb
Juseong Park <juseongkr@gmail.com>
2019-06-08 00:58:51 +0900

    Codeforces Round #564 (Div.2)

---
+++ codeforces/Codeforces_Round_564_Div2/nauuo_and_chess.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);                                                         
+	cin.tie(0);                                                                           
+	cout.tie(0);
+
+	int n, x = 1, y = 1;
+
+	cin >> n;
+	cout << n/2+1 << '\n';
+	for (int i=0; i<n; ++i) {
+		cout << x << " " << y << '\n';
+		if (x >= y)
+			y++;
+		else
+			x++;
+	}
+
+	return 0;
+}
---
+++ codeforces/Codeforces_Round_564_Div2/nauuo_and_votes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);                                                         
+	cin.tie(0);                                                                           
+	cout.tie(0);
+
+	int x, y, z;
+
+	cin >> x >> y >> z;
+	int a = x - y + z;
+	int b = x - y - z;
+	if (a < 0 && b < 0)
+		cout << "-\n";
+	else if (a > 0 && b > 0)
+		cout << "+\n";
+	else if (a == 0 && b == 0)
+		cout << "0\n";
+	else
+		cout << "?\n";
+
+	return 0;
+}
