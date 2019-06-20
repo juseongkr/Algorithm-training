1f309137fadd895987e24b15059905e4ee79a6a2
Juseong Park <juseongkr@gmail.com>
2019-06-20 15:34:08 +0900

    FIX: Find a maximum length using Parametric Search

--- acmicpc/1654.cpp
+++ acmicpc/1654.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 #define MAX 10001
 
@@ -18,15 +17,16 @@ bool check(long long l)
 
 int main()
 {
+	long long m;
 
 	cin >> k >> n;
-	for (int i=0; i<k; ++i)
+	for (int i=0; i<k; ++i) {
 		cin >> num[i];
-
-	sort(num, num+k);
+		m = max(m, num[i]);
+	}
 
 	long long ans = 0;
-	long long l = 1, r = num[k-1];
+	long long l = 1, r = m;
 	while (l <= r) {
 		long long mid = (l + r) / 2;
 		if (check(mid)) {
