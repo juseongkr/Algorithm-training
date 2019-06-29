f3bef99370b7a174d692ee5400595f8568796ab8
Juseong Park <juseongkr@gmail.com>
2019-06-30 01:11:36 +0900

    Calculate partial multiply using Segment Tree (Bottom-up)

---
+++ acmicpc/11505_2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MOD 1000000007
+
+class Segment {
+public:
+	int len;
+	vector<long long> tree;
+
+	Segment(int n) : len(n) { tree.resize(2*len); }
+
+	void make()
+	{
+		for (int i=len-1; i>0; i--)
+			tree[i] = (tree[i*2] * tree[i*2+1]) % MOD;
+	}
+
+	void update(int i, int val)
+	{
+		i += len;
+		tree[i] = val;
+		for (i/=2; i>0; i/=2)
+			tree[i] = (tree[2*i] * tree[2*i+1]) % MOD;
+	}
+
+	long long query(int l, int r)
+	{
+		l += len;
+		r += len;
+		long long ret = 1;
+		while (l <= r) {
+			if (l % 2 == 1)
+				ret = (ret * tree[l++]) % MOD;
+			if (r % 2 == 0)
+				ret = (ret * tree[r--]) % MOD;
+			l /= 2;
+			r /= 2;
+		}
+		return ret;
+	}
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m, k, a, b, c;
+
+	cin >> n >> m >> k;
+	Segment sg(n);
+	for (int i=n; i<n*2; ++i)
+		cin >> sg.tree[i];
+
+	sg.make();
+	for (int i=0; i<m+k; ++i) {
+		cin >> a >> b >> c;
+		if (a == 1)
+			sg.update(b-1, c);
+		else
+			cout << sg.query(b-1, c-1) << '\n';
+	}
+	return 0;
+}
