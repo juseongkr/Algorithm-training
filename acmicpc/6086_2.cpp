ecc6c4caadf44c2429bb2d1b0739702d2ce02055
Juseong Park <juseongkr@gmail.com>
2019-07-30 01:51:48 +0900

    Network Flow problem using Dinic Algorithm

--- acmicpc/6086_2.cpp
+++ acmicpc/6086_2.cpp
@@ -1,59 +1,9 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
+#include <climits>
 using namespace std;
-#define MAX 52
-#define INF 1000000001
-
-char a, b;
-int n, t, S, E;
-int c[MAX][MAX], f[MAX][MAX];
-int work[MAX], level[MAX];
-vector<int> graph[MAX];
-
-bool bfs()
-{
-	fill(level, level+MAX, -1);
-	level[S] = 0;
-
-	queue<int> que;
-	que.push(S);
-	while (!que.empty()) {
-		int cur = que.front();
-		que.pop();
-
-		for (int i=0; i<graph[cur].size(); ++i) {
-			int next = graph[cur][i];
-			if (level[next] == -1 && c[cur][next] - f[cur][next] > 0) {
-				level[next] = level[cur] + 1;
-				que.push(next);
-			}
-		}
-	}
-	if (level[E] == -1)
-		return false;
-	return true;
-}
-
-int dfs(int cur, int dest, int flow)
-{
-	if (cur == dest)
-		return flow;
-
-	for (int &i=work[cur]; i<graph[cur].size(); ++i) {
-		int next = graph[cur][i];
-
-		if (level[next] == level[cur] + 1 && c[cur][next] - f[cur][next] > 0) {
-			int ret = dfs(next, dest, min(flow, c[cur][next] - f[cur][next]));
-			if (ret > 0) {
-				f[cur][next] += ret;
-				f[next][cur] -= ret;
-				return ret;
-			}
-		}
-	}
-	return 0;
-}
 
 int conv(char c)
 {
@@ -62,36 +12,102 @@ int conv(char c)
 	return c-'a'+26;
 }
 
+class Dinic {
+public:
+	int len, S, E;
+	vector<vector<int>> graph, c, f;
+	vector<int> work, level;
+
+	Dinic(int n, int s, int e) : len(n), S(s), E(e)
+	{
+		work.resize(n, 0);
+		level.resize(n, 0);
+		graph.resize(n);
+		for (auto &it : graph)
+			it.resize(n, 0);
+		c.resize(n);
+		for (auto &it : c)
+			it.resize(n, 0);
+		f.resize(n);
+		for (auto &it : f)
+			it.resize(n, 0);
+	}
+
+	bool bfs()
+	{
+		for (int i=0; i<level.size(); ++i)
+			level[i] = -1;
+		level[S] = 0;
+
+		queue<int> que;
+		que.push(S);
+		while (!que.empty()) {
+			int cur = que.front();
+			que.pop();
+			for (int i=0; i<graph[cur].size(); ++i) {
+				int next = graph[cur][i];
+				if (level[next] == -1 && c[cur][next] - f[cur][next] > 0) {
+					level[next] = level[cur] + 1;
+					que.push(next);
+				}
+			}
+		}
+		return level[E] != -1;
+	}
+		
+	int dfs(int cur, int dest, int flow)
+	{
+		if (cur == dest)
+			return flow;
+
+		for (int &i=work[cur]; i<graph[cur].size(); ++i) {
+			int next = graph[cur][i];
+			if (level[next] == level[cur]+1 && c[cur][next] - f[cur][next] > 0) {
+				int ret = dfs(next, dest, min(flow, c[cur][next] - f[cur][next]));
+				if (ret > 0) {
+					f[cur][next] += ret;
+					f[next][cur] -= ret;
+					return ret;
+				}
+			}
+		}
+		return 0;
+	}
+
+	int run()
+	{
+		int total = 0;
+		while (bfs()) {
+			for (int i=0; i<work.size(); ++i)
+				work[i] = 0;
+			while (1) {
+				int flow = dfs(S, E, INT_MAX);
+				if (flow == 0)
+					break;
+				total += flow;
+			}
+		}
+		return total;
+	}
+};
+
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	char a, b;
+	int n, m, c;
 
 	cin >> n;
+	Dinic *d = new Dinic(53, conv('A'), conv('Z'));
 	for (int i=0; i<n; ++i) {
-		cin >> a >> b >> t;
+		cin >> a >> b >> c;
 		a = conv(a);
 		b = conv(b);
-		c[a][b] = c[b][a] += t;
-		graph[a].push_back(b);
-		graph[b].push_back(a);
+		d->c[a][b] = d->c[b][a] += c;
+		d->graph[a].push_back(b);
+		d->graph[b].push_back(a);
 	}
 
-	S = conv('A');
-	E = conv('Z');
-
-	int total = 0;
-	while (bfs()) {
-		fill(work, work+MAX, 0);
-		while (1) {
-			int flow = dfs(S, E, INF);
-			if (flow == 0)
-				break;
-			total += flow;
-		}
-	}
-	cout << total << '\n';
+	cout << d->run() << '\n';
 
 	return 0;
 }
