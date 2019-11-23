ececaa88bacef6476f5587a8c7c4f94c5d9e388d
Juseong Park <juseongkr@gmail.com>
2019-11-24 00:34:25 +0900

    Update: Activity selection problem using Heap

--- acmicpc/11000.cpp
+++ acmicpc/11000.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <algorithm>
-#include <functional>
-#include <vector>
 #include <queue>
 using namespace std;
 
+int n, x, y;
+priority_queue<int> que;
+vector<pair<int, int>> t;
+
 int main()
 {
-	priority_queue<int, vector<int>, greater<int>> room;
-	pair<int, int> time[200002];
-	int n;
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
 
-	scanf("%d", &n);
-	for (int i=0; i<n; ++i)
-		scanf("%d %d", &time[i].first, &time[i].second);
-
-	sort(time, time + n);
-	
-	room.push(time[0].second);
-	for (int i=1; i<n; ++i) {
-		if (room.top() <= time[i].first) {
-			room.pop();
-			room.push(time[i].second);
-		} else {
-			room.push(time[i].second);
-		}
+	cin >> n;
+	for (int i=0; i<n; ++i) {
+		cin >> x >> y;
+		t.push_back({x, y});
 	}
 
-	printf("%lu\n", room.size());
+	sort(t.begin(), t.end());
+
+	que.push(-t[0].second);
+	for (int i=1; i<n; ++i) {
+		if (-que.top() <= t[i].first)
+			que.pop();
+		que.push(-t[i].second);
+	}
+	cout << que.size() << '\n';
 
 	return 0;
 }
