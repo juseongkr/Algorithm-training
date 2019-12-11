78bfa0fbd4a317c6dfd2874be75f92afd9fcf8c6
Juseong Park <juseongkr@gmail.com>
2019-12-11 16:59:36 +0900

    Problem using next permutation

---
+++ leetcode/algorithm/31.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+	void nextPermutation(vector<int>& nums) {
+		next_permutation(nums.begin(), nums.end());
+		for (int i=0; i<nums.size(); ++i)
+			cout << nums[i] << ",";
+		cout << '\n';
+	}
+};
