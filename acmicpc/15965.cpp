0e5b6ccb71aa2240da47e7be6beb547696f46d52
Juseong Park <juseongkr@gmail.com>
2019-12-17 00:53:11 +0900

    Mathematics problem using Sieve of Eratosthenes

---
+++ acmicpc/15965.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX 10000001
+
+int n;
+bool check[MAX];
+vector<int> prime;
+
+int main()
+{
+	for (int i=2; i*i<MAX; ++i) {
+		if (check[i])
+			continue;
+		for (int j=i*i; j<MAX; j+=i)
+			check[j] = true;
+	}
+
+	for (int i=2; i<MAX; ++i)
+		if (!check[i])
+			prime.push_back(i);
+	cin >> n;
+	cout << prime[n-1] << '\n';
+
+	return 0;
+}
