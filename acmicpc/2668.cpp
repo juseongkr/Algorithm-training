0304f53ce0b4dd69067df4c16287789b68963636
Juseong Park <juseongkr@gmail.com>
2020-01-23 02:20:54 +0900

    Find a cycle problem using DFS

---
+++ acmicpc/2668.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX 101
+
+int n;
+int num[MAX];
+bool visit[MAX];
+vector<int> vec;
+
+void  dfs(int cur, int start)
+{
+	if (visit[cur] && cur == start)
+		vec.push_back(cur);
+
+	int next = num[cur];
+	if (!visit[next]) {
+		visit[next] = 1;
+		dfs(next, start);
+	}
+}
+
+int main()
+{
+	cin >> n;
+	for (int i=1; i<=n; ++i)
+		cin >> num[i];
+
+	for (int i=1; i<=n; ++i) {
+		fill(visit, visit+MAX, 0);
+		dfs(i, i);
+	}
+
+	cout << vec.size() << '\n';
+	for (int i=0; i<vec.size(); ++i)
+		cout << vec[i] << '\n';
+
+	return 0;
+}
