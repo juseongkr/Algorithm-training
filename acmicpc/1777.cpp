80d7d72d5de0d15a81e92444c248bd16db916352
Juseong Park <juseongkr@gmail.com>
2020-03-18 11:33:48 +0900

    Problem using Segment Tree with Binary Search

--- acmicpc/1777.cpp
+++ acmicpc/1777.cpp
@@ -1,29 +1,78 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 using namespace std;
 #define MAX 100001
 
-int a[MAX], b[MAX];
+int n;
+int num[MAX], ans[MAX];
+
+class Segment {
+public:
+	int len;
+	vector<int> tree;
+
+	Segment(int n) : len(n) { tree.resize(2*n); }
+
+	void make()
+	{
+		for (int i=len-1; i>0; i--)
+			tree[i] = (tree[i*2] + tree[i*2+1]);
+	}
+
+	void update(int i, int val)
+	{
+		i += len;
+		tree[i] = val;
+		for (i/=2; i>0; i/=2)
+			tree[i] = (tree[2*i] + tree[2*i+1]);
+	}
+
+	int query(int l, int r)
+	{
+		l += len;
+		r += len;
+		int ret = 0;
+		while (l <= r) {
+			if (l % 2 == 1)
+				ret += tree[l++];
+			if (r % 2 == 0)
+				ret += tree[r--];
+			l /= 2;
+			r /= 2;
+		}
+		return ret;
+	}
+};
 
 int main()
 {
-	vector<int> v;
-	int n;
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
 
 	cin >> n;
+	Segment sg(n);
 	for (int i=1; i<=n; ++i) {
-		cin >> a[i];
-		v.push_back(i);
+		cin >> num[i];
+		sg.update(i, 1);
 	}
 
-	for (int i=n; i>0; i--) {
-		b[v[a[i]]] = i;
-		v.erase(v.begin() + a[i]);
+	sg.make();
+	for (int i=n; i>=1; i--) {
+		int l = 1, r = n+1;
+		while (l <= r) {
+			int mid = (l+r)/2;
+			if (sg.query(mid, n+1) <= num[i])
+				r = mid-1;
+			else
+				l = mid+1;
+		}
+		sg.update(r, 0);
+		ans[r] = i;
 	}
 
-	for (int i=n; i>0; i--)
-		cout << b[i] << " ";
+	for (int i=1; i<=n; ++i)
+		cout << ans[i] << " ";
 
 	return 0;
 }
