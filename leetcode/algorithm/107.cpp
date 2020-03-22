b955f04cb578d4ab39bea4bb99edbfab976b228c
Juseong Park <juseongkr@gmail.com>
2020-03-22 12:10:36 +0900

    Tree Problem using BFS

---
+++ leetcode/algorithm/107.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	vector<vector<int>> levelOrderBottom(TreeNode* root) {
+		vector<vector<int>> ret;
+		queue<TreeNode*> que;
+
+		if (!root)
+			return ret;
+
+		que.push(root);
+		while (!que.empty()) {
+			ret.push_back({});
+			int sz = que.size();
+			for (int i=0; i<sz; ++i) {
+				auto cur = que.front();
+				que.pop();
+				ret.back().push_back(cur->val);
+
+				if (cur->left)
+					que.push(cur->left);
+
+				if (cur->right)
+					que.push(cur->right);
+			}
+		}
+		reverse(ret.begin(), ret.end());
+		return ret;
+	}
+};
