08bf616aae0ec2373eb64197e0f03295f0a1c060
Juseong Park <juseongkr@gmail.com>
2020-03-30 21:31:18 +0900

    kth element problem using Segment Tree

---
+++ acmicpc/2465.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 100001
+
+int n;
+int num[MAX], seq[MAX], ans[MAX];
+int tree[MAX*4];
+
+void __update(int cur, int left, int right, int idx, int diff)
+{
+	if (idx < left || right < idx)
+		return;
+
+	tree[cur] += diff;
+	if (left != right) {
+		int mid = (left + right)/2;
+		__update(2*cur, left, mid, idx, diff);
+		__update(2*cur+1, mid+1, right, idx, diff);
+	}
+}
+
+int __query(int cur, int left, int right, int k)
+{
+	if (left == right) {
+		return left;
+	} else {
+		int mid = (left + right)/2;
+		if (k <= tree[cur*2])
+			return __query(cur*2, left, mid, k);
+		else
+			return __query(cur*2+1, mid+1, right, k - tree[cur*2]);
+	}
+}
+
+void update(int i, int d)
+{
+	__update(1, 1, MAX, i, d);
+}
+
+int query(int k)
+{
+	return __query(1, 1, MAX, k);
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i=1; i<=n; ++i)
+		cin >> num[i];
+
+	sort(num+1, num+n+1);
+
+	for (int i=1; i<=n; ++i) {
+		cin >> seq[i];
+		update(i, 1);
+	}
+
+	for (int i=n; i>=1; i--) {
+		int idx = query(seq[i]+1);
+		update(idx, -1);
+		ans[i] = num[idx];
+	}
+
+	for (int i=1; i<=n; ++i)
+		cout << ans[i] << '\n';
+
+	return 0;
+}
