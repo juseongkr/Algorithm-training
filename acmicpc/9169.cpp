2764dceb5796e892a3279106cd71a9d6cb10f495
Juseong Park <juseongkr@gmail.com>
2020-04-07 14:03:09 +0900

    Minimum Cut Problem using Edmonds-Karp Algorithm

--- acmicpc/9169.cpp
+++ acmicpc/9169.cpp
@@ -1,94 +1,59 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include <queue>
-#include <climits>
 using namespace std;
 #define MAX 304
 const int src = 301, sink = 302;
+const int INF = 1e8+7;
 
-int num[MAX];
+int n, m, x, y;
+int c[MAX][MAX], f[MAX][MAX], pre[MAX];
+vector<int> graph[MAX];
 
-class NetworkFlow {
-public:
-	int len;
-	struct Edge{
-		int next, inv = 0, capa = 0;
-	};
+void add_edge(int u, int v, int capa)
+{
+	graph[u].push_back(v);
+	graph[v].push_back(u);
+	c[u][v] = c[v][u] = capa;
+}
 
-	vector<vector<Edge>> graph;
-
-	NetworkFlow(int n) : len(n)
-	{
-		graph.assign(len, vector<Edge>());
-	}
-
-	void add_edge(int u, int v, int c, int rev_c=0)
-	{
-		Edge forward, backward;
-		forward.next = v;
-		forward.capa += c;
-		forward.inv = graph[v].size();
-		backward.next = u;
-		backward.capa += rev_c;
-		backward.inv = graph[u].size();
-		graph[u].push_back(forward);
-		graph[v].push_back(backward);
-	}
-
-	int bfs(int src, int sink)
-	{
-		vector<bool> visit(len, false);
-		vector<pair<int, int >> from(len, {-1, -1});
+int flow(int src, int sink)
+{
+	int total = 0;
+	while (1) {
+		fill(pre, pre+MAX, -1);
 		queue<int> que;
-
-		visit[src] = true;
 		que.push(src);
+
 		while (!que.empty()) {
 			int cur = que.front();
 			que.pop();
-
+			if (cur == sink)
+				break;
 			for (int i=0; i<graph[cur].size(); ++i) {
-				int next = graph[cur][i].next;
-				if (!visit[next] && graph[cur][i].capa) {
-					visit[next] = true;
-					from[next] = {cur, i};
+				int next = graph[cur][i];
+				if (pre[next] == -1 && c[cur][next] > f[cur][next]) {
 					que.push(next);
+					pre[next] = cur;
 				}
 			}
 		}
-		if (!visit[sink])
-			return 0;
+		if (pre[sink] == -1)
+			break;
 
-		int p = sink;
-		int minflow = INT_MAX;
-		while (from[p].first != -1) {
-			minflow = min(minflow, graph[from[p].first][from[p].second].capa);
-			p = from[p].first;
+		int min_val = INF;
+		for (int i=sink; i!=src; i=pre[i])
+			min_val = min(min_val, c[pre[i]][i] - f[pre[i]][i]);
+
+		for (int i=sink; i!=src; i=pre[i]) {
+			f[pre[i]][i] += min_val;
+			f[i][pre[i]] -= min_val;
 		}
-
-		p = sink;
-		while (from[p].first != -1) {
-			Edge &e = graph[from[p].first][from[p].second];
-			e.capa -= minflow;
-			graph[e.next][e.inv].capa += minflow;
-			p = from[p].first;
-		}
-
-		return minflow;
+		total += min_val;
 	}
-
-	int flow(int src, int sink)
-	{
-		int total = 0;
-		while (1) {
-			int flow = bfs(src, sink);
-			if (flow == 0)
-				break;
-			total += flow;
-		}
-		return total;
-	}
-};
+	return total;
+}
 
 int main()
 {
@@ -96,36 +61,29 @@ int main()
 	cout.tie(0);
 	cin.tie(0);
 
-	int n, m, x, y;
-
 	while (1) {
 		cin >> n >> m;
 		if (n == 0 && m == 0)
 			break;
 
-		fill(num, num+MAX, 0);
-		NetworkFlow f = NetworkFlow(MAX);
-		for (int i=1; i<=n; ++i) {
+		for (int i=0; i<n; ++i) {
 			cin >> x;
-			num[i] = x;
 			if (x == 0)
-				f.add_edge(src, i, 1);
+				add_edge(src, i, 1);
 			else
-				f.add_edge(i, sink, 1);
+				add_edge(i, sink, 1);
 		}
 
-		for (int i=1; i<=m; ++i) {
+		for (int i=0; i<m; ++i) {
 			cin >> x >> y;
-			if (num[x] && !num[y]) {
-				f.add_edge(y, x, 1);
-			} else if (!num[x] && num[y]) {
-				f.add_edge(x, y, 1);
-			} else {
-				f.add_edge(x, y, 1);
-				f.add_edge(y, x, 1);
-			}
+			x--;
+			y--;
+			add_edge(x, y, 1);
 		}
-		cout << f.flow(src, sink) << '\n';
+		cout << flow(src, sink) << '\n';
+		memset(f, 0, sizeof(f));
+		memset(c, 0, sizeof(c));
+		memset(pre, 0, sizeof(pre));
 	}
 
 	return 0;
