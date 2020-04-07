f5270916e003d811b91d1133f675f831053d17e7
Juseong Park <juseongkr@gmail.com>
2020-04-07 13:59:55 +0900

    Problem for beginner

--- acmicpc/3009.py
+++ acmicpc/3009.py
@@ -1,15 +1,17 @@
 x1, y1 = input().split()
 x2, y2 = input().split()
 x3, y3 = input().split()
-if x1 == x2 and y1 == y3:
-    print(x3+' '+y2)
-elif x1 == x3 and y1 == x2:
-    print(x2+' '+y3)
-elif x2 == x1 and y2 == y3:
-    print(x3+' '+y1)
-elif x2 == x3 and y2 == y1:
-    print(x1+' '+y3)
-elif x3 == x1 and y3 == y2:
-    print(x2+' '+y1)
-elif x3 == x2 and y3 == y1:
-    print(x1+' '+y2)
+x4, y4 = 0, 0
+if x1 == x2:
+    x4 = x3
+elif x1 == x3:
+    x4 = x2
+else:
+    x4 = x1
+if y1 == y2:
+    y4 = y3
+elif y1 == y3:
+    y4 = y2
+else:
+    y4 = y1
+print(x4+' '+y4)
