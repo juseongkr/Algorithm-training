0859d7d425a82c5dcf19a97bc83ffbe3a1a94131
Juseong Park <juseongkr@gmail.com>
2020-04-16 17:34:26 +0900

    Mathematics Problem

---
+++ leetcode/30-day-leetcoding-challenge/week-3/product_of_array_except_self.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	vector<int> productExceptSelf(vector<int>& nums) {
+		vector<int> ret(nums.size(), 1);
+		int right = 1;
+
+		for (int i=1; i<nums.size(); ++i)
+			ret[i] = ret[i-1] * nums[i-1];
+
+		for (int i=nums.size()-1; i>=0; i--) {
+			ret[i] *= right;
+			right *= nums[i];
+		}
+
+		return ret;
+	}
+};
