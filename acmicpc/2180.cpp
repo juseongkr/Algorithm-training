a94542a5685d9432c3175a84c06f348e215e49d2
Juseong Park <juseongkr@gmail.com>
2020-06-14 01:18:31 +0900

    Greedy Problem using Sort

---
+++ acmicpc/2180.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#define MOD 40000
+
+int n, x, y;
+vector<pair<int, int>> vec;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i=0; i<n; ++i) {
+		cin >> x >> y;
+		vec.push_back({x, y});
+	}
+
+	sort(vec.begin(), vec.end(), [](auto x, auto y) {
+		return x.first * y.second > y.first * x.second;
+	});
+
+	int ans = 0;
+	for (const auto v : vec) {
+		ans += (v.first * ans) % MOD + v.second;
+		ans %= MOD;
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
