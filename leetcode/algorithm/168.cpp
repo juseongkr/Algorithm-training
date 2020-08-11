f83144c8cd26a6d0ac0a192eec989feb8e0a9b53
Juseong Park <juseongkr@gmail.com>
2020-08-11 12:47:16 +0900

    Brute force Problem

---
+++ leetcode/algorithm/168.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	string convertToTitle(int n) {
+		string s;
+
+		while (n) {
+			n--;
+			s += 'A' + (n % 26);
+			n /= 26;
+		}
+		reverse(s.begin(), s.end());
+
+		return s;
+	}
+};
