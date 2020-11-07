4b7ff13c001f58227ded7b3b50c0ee99877099d8
Juseong Park <juseongkr@gmail.com>
2020-11-08 01:02:29 +0900

    Graph Problem using DFS with bit masking

--- acmicpc/2001.cpp
+++ acmicpc/2001.cpp
@@ -7,8 +7,30 @@ using namespace std;
 typedef pair<int, int> pi;
 
 int n, m, k, x, y, z;
-int jewel[MAX], dist[MAX][(1 << 15)];
+int jewel[MAX];
+bool visit[MAX][(1 << 15)];
 vector<pi> graph[MAX];
+int ans;
+
+void dfs(int cur, int bit, int cnt)
+{
+	if (cur == 1 && ans < cnt)
+		ans = cnt;
+
+	visit[cur][bit] = 1;
+
+	for (int i=0; i<graph[cur].size(); ++i) {
+		int next = graph[cur][i].first;
+		int next_cost = graph[cur][i].second;
+		int next_bit = bit | (1 << jewel[cur]);
+
+		if (next_cost >= cnt && !visit[next][bit])
+			dfs(next, bit, cnt);
+
+		if (jewel[cur] && next_cost >= cnt+1 && !visit[next][next_bit])
+			dfs(next, next_bit, cnt+1);
+	}
+}
 
 int main()
 {
@@ -22,42 +44,14 @@ int main()
 		jewel[x] = i+1;
 	}
 
+	graph[1].push_back({1, (1 << 15)-1});
 	for (int i=0; i<m; ++i) {
 		cin >> x >> y >> z;
 		graph[x].push_back({y, z});
 		graph[y].push_back({x, z});
 	}
 
-	queue<pi> que;
-	dist[1][0] = 1;
-	que.push({1, 0});
-
-	int ans = 0;
-	while (!que.empty()) {
-		auto [cur, bit] = que.front();
-		que.pop();
-
-		if (cur == 1)
-			ans = max(ans, dist[cur][bit]);
-
-		if (jewel[cur] && !(bit & (1 << jewel[cur]))) {
-			int next_bit = bit | (1 << jewel[cur]);
-			if (dist[cur][next_bit] < dist[cur][bit] + 1) {
-				dist[cur][next_bit] = dist[cur][bit] + 1;
-				que.push({cur, next_bit});
-			}
-		}
-
-		for (int i=0; i<graph[cur].size(); ++i) {
-			int next = graph[cur][i].first;
-			int next_cost = graph[cur][i].second;
-
-			if (dist[cur][bit] <= next_cost && dist[next][bit] < dist[cur][bit]) {
-				dist[next][bit] = dist[cur][bit];
-				que.push({next, bit});
-			}
-		}
-	}
+	dfs(1, 0, 0);
 
 	cout << ans << '\n';
 
