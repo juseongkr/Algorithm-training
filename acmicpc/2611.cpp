df35f4337f6933d6c8ff8cf65e849943c7cb7bb6
Juseong Park <juseongkr@gmail.com>
2020-11-11 09:57:23 +0900

    Graph Problem using Dynamic Programming

--- acmicpc/2611.cpp
+++ acmicpc/2611.cpp
@@ -1,52 +1,55 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
-#include <queue>
-#include <stack>
 using namespace std;
 #define MAX 1001
 
 int n, m, x, y, k;
 vector<pair<int, int>> graph[MAX];
-int dist[MAX], pre[MAX];
+int dp[MAX], path[MAX];
+
+int solve(int cur)
+{
+	if (~dp[cur])
+		return dp[cur];
+
+	dp[cur] = 0;
+	for (int i=0; i<graph[cur].size(); ++i) {
+		int next = graph[cur][i].first;
+		int cost = graph[cur][i].second;
+		int next_cost = solve(next) + cost;
+
+		if (dp[cur] < next_cost) {
+			dp[cur] = next_cost;
+			path[cur] = next;
+		}
+	}
+
+	return dp[cur];
+}
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+
 	cin >> n >> m;
 	for (int i=0; i<m; ++i) {
 		cin >> x >> y >> k;
-		graph[x].push_back({y, k});
+		if (y == 1)
+			graph[x].push_back({n+1, k});
+		else
+			graph[x].push_back({y, k});
 	}
 
-	priority_queue<pair<int, int>> que;
-	fill(dist, dist+MAX, -1e9+7);
-	dist[1] = 0;
-	que.push({0, 1});
+	memset(dp, -1, sizeof(dp));
+	cout << solve(1) << '\n';
 
-	while (!que.empty()) {
-		auto [cost, cur] = que.top();
-		que.pop();
-
-		for (int i=0; i<graph[cur].size(); ++i) {
-			int next = graph[cur][i].first;
-			int next_cost = graph[cur][i].second + cost;
-
-			if (dist[next] < next_cost) {
-				dist[next] = next_cost;
-				pre[next] = cur;
-				if (next != 1)
-					que.push({next_cost, next});
-			}
-		}
-	}
-	stack<int> st;
-	cout << dist[1] << '\n';
-	for (int i=pre[1]; i!=1; i=pre[i])
-		st.push(i);
-
-	cout << "1 ";
-	while (!st.empty()) {
-		cout << st.top() << " ";
-		st.pop();
+	int idx = 1;
+	while (idx <= n) {
+		cout << idx << " ";
+		idx = path[idx];
 	}
 	cout << "1\n";
 
