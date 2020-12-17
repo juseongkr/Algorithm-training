18f24deed0bf9eef568c9c11542ee9f08806ffb9
Juseong Park <juseongkr@gmail.com>
2020-12-17 11:41:06 +0900

    Graph Problem using Dynamic Programming

--- acmicpc/2157.cpp
+++ acmicpc/2157.cpp
@@ -1,40 +1,53 @@
 #include <iostream>
 #include <cstring>
-#include <climits>
+#include <vector>
 using namespace std;
+const int INF = 1e9+7;
 #define MAX 301
 
 int n, m, k, a, b, c;
 int map[MAX][MAX], dp[MAX][MAX];
+vector<int> graph[MAX];
 
 int solve(int cur, int cnt)
 {
 	if (cur == n)
 		return 0;
-	if (cnt == m)
-		return INT_MIN;
+	if (cnt == 1)
+		return map[cur][n] ? map[cur][n] : -INF;
 
-	if (dp[cur][cnt] != -1)
+	if (~dp[cur][cnt])
 		return dp[cur][cnt];
 
-	dp[cur][cnt] = 0;
-	for (int i=cur+1; i<=n; ++i) {
-		if (map[cur][i])
-			dp[cur][cnt] = max(dp[cur][cnt], solve(i, cnt+1) + map[cur][i]);
+	dp[cur][cnt] = -INF;
+	for (int i=0; i<graph[cur].size(); ++i) {
+		int next = graph[cur][i];
+		int value = solve(next, cnt-1);
+
+		if (value != -INF)
+			dp[cur][cnt] = max(dp[cur][cnt], value + map[cur][next]);
 	}
+
 	return dp[cur][cnt];
 }
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+
 	cin >> n >> m >> k;
 	for (int i=0; i<k; ++i) {
 		cin >> a >> b >> c;
-		map[a][b] = max(map[a][b], c);
+		if (a < b) {
+			map[a][b] = max(map[a][b], c);
+			graph[a].push_back(b);
+		}
 	}
 
 	memset(dp, -1, sizeof(dp));
-	cout << solve(1, 1) << '\n';
+	cout << solve(1, m-1) << '\n';
 
 	return 0;
 }
