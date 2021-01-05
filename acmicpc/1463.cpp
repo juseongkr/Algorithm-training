67cf321dc1ce7b4a742ddf4ba07a12945e88ffe7
Juseong Park <juseongkr@gmail.com>
2021-01-05 12:11:54 +0900

    Problem using Dynamic Programming

--- acmicpc/1463.cpp
+++ acmicpc/1463.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
+#define MAX 1000001
+
+int n;
+int dp[MAX];
+
+int solve(int n)
+{
+	if (n == 1)
+		return 0;
+
+	if (~dp[n])
+		return dp[n];
+
+	int ret = solve(n-1) + 1;
+	if (n % 3 == 0)
+		ret = min(ret, solve(n/3) + 1);
+	if (n % 2 == 0)
+		ret = min(ret, solve(n/2) + 1);
+	
+	return dp[n] = ret;
+}
 
 int main()
 {
-	long long memo[1000001];
-	long long num;
+	cin >> n;
 
-	scanf("%lld", &num);
-	memo[1] = 0;
-	for (int i=2; i<=num; ++i) {
-		memo[i] = memo[i-1] + 1;
-		if (i % 3 == 0)
-			memo[i] = min(memo[i], memo[i/3]+1);
-		else if (i % 2 == 0)
-			memo[i] = min(memo[i], memo[i/2]+1);
-	}
-
-	printf("%lld\n", memo[num]);
+	fill(dp, dp+MAX, -1);
+	cout << solve(n) << '\n';
 
 	return 0;
 }
