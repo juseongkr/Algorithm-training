c8d49f8dabec6aa2d8036c442b4688c86c9d4b9f
Juseong Park <juseongkr@gmail.com>
2021-05-19 11:20:57 +0900

    Problem using Binary Search

---
+++ leetcode/algorithm/34.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+	vector<int> searchRange(vector<int>& nums, int target) {
+		int left = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+		int right = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+
+		if (left > right-1)
+			return {-1, -1};
+
+		return {left, right-1};
+	}
+};
